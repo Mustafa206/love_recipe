8f855c6e32ac190b07cb2cf5289dadc898e2ec92
mustafa206 <mustafa.ahmed.007@gmail.com>
2015-07-26 20:24:15 -0700

    another commit

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt'
  gem 'bootstrap_form'
  gem 'bootstrap-sass'
  gem "autoprefixer-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
 
   get 'users/favorites'
 
+
+  get "login" => "sessions#new"
+  post "login" => "session#create"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
