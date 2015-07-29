35eaa2ab1b79ab66456a03148fe37d5e1a85d285
mustafa206 <mustafa.ahmed.007@gmail.com>
2015-07-29 10:06:58 -0700

    new commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      redirect_to @user
+
+      redirect_to login_path
     else
       render 'new'
     end
