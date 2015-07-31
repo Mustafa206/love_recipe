f3cec43b5a573f2a70566300e617cac3d75d968a
mustafa206 <mustafa.ahmed.007@gmail.com>
2015-07-30 19:42:52 -0700

    commit

--- app/assets/javascripts/background.js
+++ app/assets/javascripts/background.js
@@ -9,14 +9,17 @@ jQuery(function($) {
         slides           : [
 
             {image : 'assets/bg1.jpg'},
-            {image : 'assets/bg3.jpg'},
             {image : 'assets/bg4.jpg'},
             {image : 'assets/bg5.jpg'},
             {image : 'assets/bg6.jpg'},
             {image : 'assets/bg7.jpg'},
             {image : 'assets/bg8.jpg'},
-            {image : 'assets/bg9.jpg'},
-            {image : 'assets/bg10.jpg'}
+            {image : 'assets/bg10.jpg'},
+            {image : 'assets/bg11.jpg'},
+            {image : 'assets/bg13.jpg'},
+            {image : 'assets/bg14.jpg'},
+            {image : 'assets/bg15.jpg'}
+
         ]
 
     });
